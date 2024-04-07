144c4df479facab74eccdf467b5a75dc93dc77ba
Oleksandr Krasnoshchokov <krasnoshchokov.dev@gmail.com>
2024-04-08 00:03:28 +0300

    storybook is added

---
+++ client/src/components/general/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+
+import Avatar from './Avatar';
+
+export default {
+  component: Avatar,
+  title: 'Avatar',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      title: 'Avatar',
+      state: 'AVATAR_INBOX',
+    },
+  },
+};
+
---
+++ client/src/components/general/LoadingSpinner.stories.tsx
@@ -0,0 +1,20 @@
+import LoadingSpinner from "./LoadingSpinner";
+
+
+
+export default {
+  component: LoadingSpinner,
+  title: 'LoadingSpinner',
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: '1',
+      title: 'LoadingSpinner',
+      state: 'LOADING_SPINNER_INBOX',
+    },
+  },
+};
+
