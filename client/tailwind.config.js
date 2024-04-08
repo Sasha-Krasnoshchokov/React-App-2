a492243a3cbe03087176b84b8e2722fd967e913f
Oleksandr Krasnoshchokov <krasnoshchokov.dev@gmail.com>
2024-04-08 17:07:06 +0300

    fix prod congfigs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,35 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px',
-    },
-    colors: {
-      'gray-dark': '#273444',
-      gray: '#8492a6',
-      'gray-light': '#d3dce6',
-      white: '#fff',
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
     extend: {
-      spacing: {
-        128: '32rem',
-        144: '36rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        '2xl': '1536px',
       },
-      borderRadius: {
-        none: '0',
-        sm: '.125rem',
-        DEFAULT: '.25rem',
-        lg: '.5rem',
-        full: '9999px',
+      colors: {
+        'gray-dark': '#273444',
+        gray: '#8492a6',
+        'gray-light': '#d3dce6',
+        white: '#fff',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          128: '32rem',
+          144: '36rem',
+        },
+        borderRadius: {
+          none: '0',
+          sm: '.125rem',
+          DEFAULT: '.25rem',
+          lg: '.5rem',
+          full: '9999px',
+        },
       },
     },
   },
